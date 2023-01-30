dd785b3560211d3a3e6b85617ba23ea5b55080b1
andres <andresescalante1911@gmail.com>
2023-01-30 18:53:10 -0500

    prueba

--- prueba/autores/models.py
+++ prueba/autores/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+#Modelo para la entidad Autor. 
+class Autor(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    pais = models.CharField(max_length=30)
