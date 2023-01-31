ecbefd886d53659edc10f407cd791d00a304fbe7
andres <andresescalante1911@gmail.com>
2023-01-30 19:52:16 -0500

    holi

---
+++ prueba/autores/forms.py
@@ -0,0 +1,17 @@
+
+from django import forms
+from .models import Autor
+
+#Crear formulario
+class AutorForm(forms.ModelForm):
+
+    #metaclase
+    class Meta:
+        model = Autor
+
+        #especificar los campos
+        fields = [
+            'nombre',
+            'apellido',
+            'pais'
+        ]
--- prueba/prueba/urls.py
+++ prueba/prueba/urls.py
@@ -1,5 +1,4 @@
-"""prueba URL Configuration
-
+"""libreria URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
@@ -14,8 +13,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
+    path('',include('libros.urls')),
+    path('autor/',include('autores.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
